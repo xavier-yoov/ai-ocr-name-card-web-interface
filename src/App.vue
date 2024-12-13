1b60f6c18be98f1a2270df45692f0bb1ecdfad48
Xavier Au <xavier.au@gmail.com>
2024-12-13 23:43:18 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img :src="YOOVLogo" class="d-inline-block align-text-top">
+      </a>
+    </div>
   </nav>
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup lang="ts">
+import YOOVLogo from '@/assets/yoov-logo.png'
+</script>
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+<style scoped>
+.navbar .navbar-brand img{
+  width: 8rem;
+  max-width: 200px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,164 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+   <div class="row preview-container d-flex align-items-center">
+     <div class="col text-center"
+     v-for="file in files"
+     :key="file.id">
+       <div class="position-relative">
+       <img :src="file.src" class="img-fluid" >
+       <button :disabled="isLoading" class="btn btn-sm btn-danger delete-button shadow"
+       v-on:click="removeFile(file)">
+         <i class="fa fa-times" aria-hidden="true"></i>
+       </button>
+       </div>
+     </div>
+   </div>
+
+    <form ref="createForm"
+          class="form mt-5 text-center">
+      <div class="form-group">
+        <input type="file"
+               accept="image/*"
+               name="files"
+               multiple class="d-none form-control"
+               id="input"
+               ref="fileInput"
+               v-on:change="updateImages"
+               placeholder="Enter input" />
+        <div class="d-flex justify-content-center gap-4">
+          <button type="button"
+                  :disabled="isLoading"
+                  class="btn btn-primary py-3 px-5 shadow"
+                  v-on:click="selectImages">Select</button>
+          <button type="button"
+                  v-on:click.prevent="submitNameCard"
+                  v-if="files.length"
+                  :disabled="isLoading"
+                  class="btn btn-success shadow d-flex align-items-center gap-2"
+                  :class="{'py-1 px-3':isLoading, 'py-3 px-5':!isLoading}">
+            <div class="d-flex align-items-center gap-2" v-if="isLoading">
+              <div class="spinner-border text-light"
+                   role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
+              Processing...
+            </div>
+            <span v-else>Submit</span>
+            </button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
 
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-});
+import {ref} from 'vue';
+import {useRoute} from "vue-router";
+import axios from "axios";
+
+const route = useRoute()
+
+const user = route.query.user as string
+
+const createForm = ref<HTMLFormElement|null>(null)
+
+const fileInput = ref<HTMLInputElement | null>(null)
+
+const isLoading = ref(false)
+
+const files = ref<{id:string, src:any, mimeType:string}[]>([])
+
+const selectImages = ()=>{
+  if(fileInput.value){
+    fileInput.value.click()
+  }
+}
+
+const updateImages = ()=>{
+  console.log("files", fileInput.value?.files)
+
+  if(fileInput.value?.files){
+    for(let i=0; i<fileInput.value.files.length; i++){
+      const file = fileInput.value.files[i]
+      const reader = new FileReader()
+      reader.onload = (e)=>{
+        files.value.push({
+          id:Math.random().toString(36).substr(2, 9),
+          src: URL.createObjectURL(file),
+          mimeType: file.type
+        })
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+}
+
+const removeFile = (file:any)=>{
+  const index = files.value.findIndex(f=>f.id === file.id)
+  if(index > -1){
+    files.value.splice(index, 1)
+  }
+}
+
+const submitNameCard = async()=>{
+
+  isLoading.value = true
+
+  const base_url = process.env.VUE_APP_SERVICE_ENDPOINT
+
+  const instance = axios.create({
+    baseURL: base_url
+  })
+
+  const formData = new FormData(createForm.value!)
+
+  try{
+
+    const response = await instance.post('/ocr',formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+
+    const url = response.data.url
+
+    const link = document.createElement('a')
+    link.href = url
+    const fileName = url.split('/').pop()
+    link.setAttribute('download', fileName)
+    link.target = '_blank'
+    document.body.appendChild(link)
+    link.click()
+    files.value = []
+
+  }catch (e) {
+    console.error(e)
+    alert("Something went wrong. Try again later")
+  }finally {
+    isLoading.value = false
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.preview-container img {
+  max-height: 350px;
 }
-ul {
-  list-style-type: none;
+
+button.delete-button{
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  width: 2rem;
+  height: 2rem;
+  line-height: 2rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  border-radius: 50%;
+  margin: 0;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  text-align: center;
 }
 </style>
